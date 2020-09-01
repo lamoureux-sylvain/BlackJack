f73e0792bf2ee64071a0fe29cfb8c8f9f27caae0
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-09-01 13:39:11 +0200

    création des fichiers, ajout du cas même score, premières arborescence

---
+++ blackjack.py
@@ -0,0 +1,16 @@
+class blackjack:
+    def __init__(self):
+        pass
+
+    def new_game(self):
+        pass
+    
+    def rules(self):
+        pass
+    
+    #def leaderboard(self): 
+
+    def quit(self):
+        return exit()
+
+
