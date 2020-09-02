39fd3af0ce0dd9b7012eaf9b31f0036694189640
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2020-09-02 14:47:10 +0200

    modification retour au menu proincipal

--- menu.py
+++ menu.py
@@ -1,15 +1,17 @@
 class Menu:
     def __init__(self):
         pass
-    
+
     def main_title(self):
         self.player_choice = ""
-        self.choices = ["n","r","c","q"]
+        self.choices = ["n", "r", "c", "q"]
         print("""                          BLACK JACK!!""")
         print("------------------------------------------------------------------\n")
         print("- Nouvelle partie (N)\n- Règles (R)\n- Classement (C)\n- Quitter (Q)\n")
         while self.player_choice not in self.choices:
-            self.player_choice = input("Bienvenue ! Que souhaitez vous faire ? ").lower()
+            self.player_choice = input(
+                "Bienvenue ! Que souhaitez vous faire ? "
+            ).lower()
 
         if self.player_choice == "n":
             Menu().new_game()
@@ -23,29 +25,29 @@ class Menu:
     def new_game(self):
         print("""\n                     NOUVELLE PARTIE !!""")
         print("------------------------------------------------------------------\n")
-        
 
-        
     def rules(self):
         self.player_choice = ""
         print("""\n                 Les règles du Black Jack !!""")
         print("------------------------------------------------------------------\n")
         print("Va voir sur Google ce sera plus simple !\n")
-        while self.player_choice != "q":
-            self.player_choice = input("Menu principal (Q) : ").lower()
-        if self.player_choice == "q":
+        while self.player_choice != "m":
+            self.player_choice = input("Menu principal (M) : ").lower()
+        if self.player_choice == "m":
             Menu().main_title()
-    
+
     def leaderboard(self):
         self.player_choice = ""
         print("""\n                         CLASSEMENT""")
         print("------------------------------------------------------------------\n")
-        print("""                    1. Éléonore D'Herbigny
+        print(
+            """                    1. Éléonore D'Herbigny
                     2. Cyril Simonin
                     3. Syvain Lamoureux
                     3. Alexandre Vilbert
                     3. Étienne Rist
-                    4. Rolique Oponga\n""")
+                    4. Rolique Oponga\n"""
+        )
         while self.player_choice != "q":
             self.player_choice = input("Menu principal (Q) : ").lower()
         if self.player_choice == "q":
@@ -53,5 +55,3 @@ class Menu:
 
     def quit(self):
         exit()
-
-
