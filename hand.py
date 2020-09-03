a04fd58e809a64acd61b2ec2b119bcaf3dd6fc0e
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2020-09-03 16:21:48 +0200

    correction lgique du croupier entre 17 et 21

--- hand.py
+++ hand.py
@@ -6,25 +6,31 @@ class Hand:
         self.dealer = dealer
         self.cards = []
         self.value = 0
+        self.ace_value = 0
 
     def add_card(self, card):
         self.cards.append(card)
 
     def calculate_value(self):
         self.value = 0
-        has_ace = False
+        # has_ace = False
         for card in self.cards:
             if card.value.isnumeric():
                 self.value += int(card.value)
+
             else:
                 if card.value == "A":
-                    has_ace = True
-                    self.value += 11
+                    while (self.ace_value != "1") and (self.ace_value != "11"):
+                        self.ace_value = input(
+                            "Quelle valeur donnez vous à l'As ? 1 ou 11 ?: "
+                        )
+                    self.value += int(self.ace_value)
+
                 else:
                     self.value += 10
 
-        if has_ace and self.value > 21:
-            self.value -= 10
+        # if has_ace and self.value > 21:
+        #     self.value -= 10
 
     def get_value(self):
         self.calculate_value()
@@ -35,6 +41,7 @@ class Hand:
             print("carte cachée")
             print(self.cards[0])
             print(self.cards[1])
+
         else:
             for card in self.cards:
                 print(card)
