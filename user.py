113fdb5b74772926b11796109c955c68a1e6989f
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2020-09-04 12:06:19 +0200

    mise en page démarrage partie pseudo + attribution credit

--- user.py
+++ user.py
@@ -5,16 +5,26 @@ class User:
 
     def generate_player(self):
         for number in range(self.nb_players):
+            print(
+                r"""+===================================================================+ """
+            )
             self.players.append(
                 input("J" + str(number + 1) + "... Quel est votre pseudo ? ")
             )
         if len(self.players) == 1:
-            print("Enchanté", self.players[0],"!")
-            print("Pour fêter notre rencontre voici 1000 jetons !!")
+            print(
+                r"""+-------------------------------------------------------------------+"""
+            )
+            print("\n                          Enchanté", self.players[0], "!")
+            print("           Pour fêter notre rencontre voici 1000 jetons !\n")
+            print(
+                r"""+-------------------------------------------------------------------+"""
+            )
             return User(self.players)
         else:
             print("Enchanté", ", ".join(self.players), "!")
             print("Pour fêter notre rencontre voici 1000 jetons !!")
+
 
 if __name__ == "__main__":
     user = User([])
